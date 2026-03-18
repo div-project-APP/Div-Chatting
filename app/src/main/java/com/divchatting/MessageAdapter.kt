af405322562f19aab2c977e9c805834693524647
div-project-APP <diviganteng01@gmail.com>
2026-03-19 00:21:32 +0700

    Create MessageAdapter.kt

---
+++ app/src/main/java/com/divchatting/MessageAdapter.kt
@@ -0,0 +1,32 @@
+package com.divchatting
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+data class Message(val sender: String, val content: String)
+
+class MessageAdapter(private val messages: List<Message>) :
+    RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
+
+    class MessageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val sender: TextView = view.findViewById(R.id.textMessageSender)
+        val content: TextView = view.findViewById(R.id.textMessageContent)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_message, parent, false)
+        return MessageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val msg = messages[position]
+        holder.sender.text = msg.sender
+        holder.content.text = msg.content
+    }
+
+    override fun getItemCount(): Int = messages.size
+}
